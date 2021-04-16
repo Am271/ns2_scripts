d51e9dbbb0e69cdfe8839fe5ec0c63ee91dcf33e
Amitash M S <amitu895@gmail.com>
2021-04-17 00:17:42 +0530

    added one more node to network

--- sim.tcl
+++ sim.tcl
@@ -13,24 +13,35 @@ proc finish {} {
 
 set n0 [$ns node]
 set n1 [$ns node]
+set n2 [$ns node]
 
 $ns duplex-link $n0 $n1 1Mb 10ms DropTail
+$ns duplex-link $n1 $n2 1Mb 10ms DropTail
 
 set udp0 [new Agent/UDP]
 $ns attach-agent $n0 $udp0
+set udp1 [new Agent/UDP]
+$ns attach-agent $n1 $udp1
+set udp2 [new Agent/UDP]
+$ns attach-agent $n2 $udp2
 
 set cbr0 [new Application/Traffic/CBR]
 $cbr0 set packetSize_ 500
 $cbr0 set interval_ 0.005
 $cbr0 attach-agent $udp0
 
-set null0 [new Agent/Null]
-$ns attach-agent $n1 $null0
+set cbr1 [new Application/Traffic/CBR]
+$cbr1 set packetSize_ 250
+$cbr1 set interval_ 0.005
+$cbr1 attach-agent $udp2
 
-$ns connect $udp0 $null0
+$ns connect $udp0 $udp1
+$ns connect $udp2 $udp1
 
 $ns at 0.5 "$cbr0 start"
+$ns at 0.5 "$cbr1 start"
 $ns at 4.5 "$cbr0 stop"
+$ns at 4.5 "$cbr1 stop"
 
 $ns at 5.0 "finish"
 
